5c264b223bec765851ee1013c8e0f8fd2ae86494
Manho Joung <manhojoung@gmail.com>
2023-08-03 20:52:20 +0000

    pages app

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def home_page_view(request):
-    return HttpResponse("Mambo Power!")
+class HomePageView(TemplateView):
+    template_name = "home.html"
